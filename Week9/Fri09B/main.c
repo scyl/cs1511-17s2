565b67dd4988a2c40c4771ca9d8129ad9ec1fbd1
Stephen Ching Yiu Leung <scyl@cse.edu.au>
2017-09-22 10:24:59 +1000

    Wk9 Fri

---
+++ Week9/Fri09B/main.c
@@ -0,0 +1,14 @@
+// Comments
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Card.h"
+#include <assert.h>
+
+int main(int argc, char *argv[]) {
+
+    Card card1 = newCard(3, GREEN, DIAMONDS);
+    assert(cardSuit(card1) == DIAMONDS);
+
+    return EXIT_SUCCESS;
+}
---
+++ Week9/Thu16A/test.c
@@ -0,0 +1,21 @@
+// Comments
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef enum {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    PURPLE
+} color;
+
+int main(int argc, char *argv[]) {
+
+    color hi = 0;
+    printf("%d\n", hi);
+
+    return EXIT_SUCCESS;
+}
